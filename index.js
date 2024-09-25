2186f4110ad501518501b987d81c3c959d2f5fb6
Yusuf Abdul <yusufabdul212112@gmail.com>
2024-09-25 08:13:33 +0300

    adds new file

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000){
+    return 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <=2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500){
+    return 'No can do.'
+  }
+}
+function ternaryCheckCity(city){
+  return city === 'NYC'?'Ok, sounds good.' :'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return 'Bye.'
+  }
 }
